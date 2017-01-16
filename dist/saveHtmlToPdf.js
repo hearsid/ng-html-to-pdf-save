bd1d573379ae15b55f5076a6e8edc7cbe6781576
VijayKumar Dogra <vijaydogra@gmail.com>
2017-01-16 23:37:13 +0530

    Fix for blank PDF (#10)
    * Support for multiple pages
    
    * Fix for blank PDF

--- dist/saveHtmlToPdf.js
+++ dist/saveHtmlToPdf.js
@@ -89,8 +89,51 @@ angular.module('htmlToPdfSave')
 
 
 					function convert(theElement , id) {
+						var quotes = $('div[pdf-save-content='+id+']')[0];
+				        html2canvas(quotes, {
+				            onrendered: function(canvas) {
+				            var pdf = new jsPDF('p', 'pt', 'letter');
+				            for (var i = 0; i <= quotes.clientHeight/980; i++) {
+				                var srcImg  = canvas;
+				                var sX      = 0;
+				                var sY      = 980*i; // start 980 pixels down for every new page
+				                var sWidth  = 900;
+				                var sHeight = 980;
+				                var dX      = 0;
+				                var dY      = 0;
+				                var dWidth  = 900;
+				                var dHeight = 980;
 
-						var element = $('[pdf-save-content='+id+']') ,
+				                window.onePageCanvas = document.createElement("canvas");
+				                onePageCanvas.setAttribute('width', 900);
+				                onePageCanvas.setAttribute('height', 980);
+				                var ctx = onePageCanvas.getContext('2d');
+				                // details on this usage of this function: 
+				                // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Using_images#Slicing
+				                ctx.drawImage(srcImg,sX,sY,sWidth,sHeight,dX,dY,dWidth,dHeight);
+
+				                // document.body.appendChild(canvas);
+				                var canvasDataURL = onePageCanvas.toDataURL("image/png", 1.0);
+
+				                var width         = onePageCanvas.width;
+				                var height        = onePageCanvas.clientHeight;
+
+				                //! If we're on anything other than the first page,
+				                // add another page
+				                if (i > 0) {
+				                    pdf.addPage(612, 791); //8.5" x 11" in pts (in*72)
+				                }
+				                //! now we declare that we're working on that page
+				                pdf.setPage(i+1);
+				                //! now we add content to that page!
+				                pdf.addImage(canvasDataURL, 'PNG', 20, 40, (width*.62), (height*.62));
+
+				            }
+				            //! after the for loop is finished running, we save the pdf.
+				            pdf.save(broadcastedName);
+				        }
+				      });
+						/*var element = $('[pdf-save-content='+id+']') ,
 						cache_width = element.width(),
 						 a4  =[ 595.28,  841.89];  // for a4 size paper width and height
 
@@ -121,7 +164,7 @@ angular.module('htmlToPdfSave')
 								imageTimeout:2000,
 								removeContainer:true
 							});
-						}
+						}*/
 
 
 					}
